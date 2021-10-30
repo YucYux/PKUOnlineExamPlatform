ba52135a29f5a0654a806414d014d49ca65d90f0
yucyux <yucyux@outlook.com>
2021-10-30 09:19:52 -0700

    复用QDU部分代码，添加了部分必要的库和类型

--- vue/src/components/HelloWorld.vue
+++ vue/src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="hello">
+    <Navi></Navi>
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
     <ul>
@@ -84,12 +85,17 @@
 </template>
 
 <script>
+import Navi from "./Navi.vue"
+
 export default {
   name: 'HelloWorld',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+  components:{
+    Navi
   }
 }
 </script>
