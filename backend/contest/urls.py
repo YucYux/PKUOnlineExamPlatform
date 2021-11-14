f63b712a95d21660c2e4a19dd17aad8a18d1d4b3
coolzifan123 <2814299761@qq.com>
2021-11-14 18:17:42 +0800

    修改一些错误

--- backend/contest/urls.py
+++ backend/contest/urls.py
@@ -2,13 +2,13 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from rest_framework.routers import DefaultRouter
 from ..contest import views
-
+from views import ContestAnnouncementListAPI,ContestAccessAPI, ContestRankAPI,ContestAPI
 
 router = DefaultRouter()
 router.register(r'contest', views.ContestViewSet)
 
 urlpatterns = [
-    url(r'contest', include(router.urls)),
+    url(r'contest', ContestAPI.as_view()),
     url(r"^contest/announcement/?$", ContestAnnouncementListAPI.as_view(), name="contest_announcement_api"),
     url(r"^contest/access/?$", ContestAccessAPI.as_view(), name="contest_access_api"),
     url(r"^contest_rank/?$", ContestRankAPI.as_view(), name="contest_rank_api"),
