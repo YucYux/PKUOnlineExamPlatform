64050530a56d11200738e696ca9f7d033fb089d2
coolzifan123 <2814299761@qq.com>
2021-11-14 18:21:19 +0800

    修改一些错误

--- backend/contest/urls.py
+++ backend/contest/urls.py
@@ -4,11 +4,11 @@ from rest_framework.routers import DefaultRouter
 from ..contest import views
 from views import ContestAnnouncementListAPI,ContestAccessAPI, ContestRankAPI,ContestAPI
 
-router = DefaultRouter()
-router.register(r'contest', views.ContestViewSet)
+# router = DefaultRouter()
+# router.register(r'contest', views.ContestViewSet)
 
 urlpatterns = [
-    url(r'contest', ContestAPI.as_view()),
+    url('contest', ContestAPI.as_view()),
     url(r"^contest/announcement/?$", ContestAnnouncementListAPI.as_view(), name="contest_announcement_api"),
     url(r"^contest/access/?$", ContestAccessAPI.as_view(), name="contest_access_api"),
     url(r"^contest_rank/?$", ContestRankAPI.as_view(), name="contest_rank_api"),
