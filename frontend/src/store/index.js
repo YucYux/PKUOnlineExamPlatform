d6e720fdf2fbe7bd949344589a96bc0b33d7f795
yucyux <yucyux@outlook.com>
2021-11-15 04:17:01 -0800

    登录api

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -4,7 +4,8 @@ Vue.use(Vuex);
 
 const state={
     access: '',
-    refresh: ''
+    refresh: '',
+    username: ''
 };
 const getters = {   
     getAccess(state) {
@@ -12,14 +13,36 @@ const getters = {
     },
     getRefresh(state) {
         return state.refresh;
+    },
+    getUsername(state) {
+        return state.username;
     }
 };
 const mutations = {
-    changeAccess(state, newAccess) {
+    changeAccessM(state, newAccess) {
         state.access = newAccess;
     },
-    changeRefresh(state, newRefresh) {
+    changeRefreshM(state, newRefresh) {
         state.refresh = newRefresh;
+    },
+    changeUsernameM(state, newUsername) {
+        state.username = newUsername;
+    }
+};
+const actions = {
+    changeAccess(context, newAccess) {
+        context.commit('changeAccessM', newAccess);
+    },
+    changeRefresh(context, newRefresh) {
+        context.commit('changeRefreshM', newRefresh);
+    },
+    changeUsername(context, newUsername) {
+        context.commit('changeUsernameM', newUsername);
+    },
+    storeInfoWhenLogin(context, newAccess, newRefresh, newUsername) {
+        context.commit('changeAccessM', newAccess);
+        context.commit('changeRefreshM', newRefresh);
+        context.commit('changeUsernameM', newUsername);
     }
 };
 const store = new Vuex.Store({
