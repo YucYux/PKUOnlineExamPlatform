903a8b04aae22b690ba792c95f663ca52fa5cd94
yucyux <yucyux@outlook.com>
2021-11-16 09:14:26 -0800

    班级界面进一步完善

---
+++ frontend/src/components/class_list.vue
@@ -0,0 +1,31 @@
+<template>
+  <Scroll :on-reach-bottom="handleReachBottom" height="560">
+    <Class v-for="(item, index) in list_name" :key="index" :class_name="list_name[index]" :class_id="list_info[index]"></Class>
+  </Scroll>
+</template>
+<script>
+import Class from './class_list_element.vue'
+export default {
+  data () {
+    return {
+      list_name: ['class1', 'class2', 'class3'],
+      list_info: ['0001', '0002', '0003'],
+    }
+  },
+  methods: {
+    handleReachBottom () {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve()
+        }, 2000)
+      })
+    }
+  },
+  mounted: function () {
+    // 初始时从后端获取数据更新contest列表
+  },
+  components: {
+    Class
+  }
+}
+</script>
--- frontend/src/components/class_list_element.vue
+++ frontend/src/components/class_list_element.vue
@@ -1,34 +1,25 @@
 <template>
   <div style="padding: 10px; background: #f8f8f9">
     <Card
-      :title="contest_name"
-      icon="ios-trophy"
+      :title="class_name"
+      icon="md-contacts"
       padding="0"
       shadow
       style="width: 100%"
       to="#"
     >
       <CellGroup>
-        <Cell :title="contest_info"></Cell>
-        <Cell>
-          <Icon type="md-calendar" />{{
-            contest_date
-          }}&nbsp;&nbsp;&nbsp;&nbsp;<Icon type="md-clock" />{{
-            contest_time
-          }}
-          </Cell>
+        <Cell :title="class_id"></Cell>
       </CellGroup>
     </Card>
   </div>
 </template>
 <script>
 export default {
-  props: ['contest_name', 'contest_info', 'contest_date', 'contest_time'],
+  props: ['class_name', 'class_id'],
   data () {
     return {
-      informations: 'exam-informations',
-      exam_date: 'xxxx-xx-xx',
-      time_limit: 'xx:xx -- xx:xx'
+      id: 0
     }
   }
 }
