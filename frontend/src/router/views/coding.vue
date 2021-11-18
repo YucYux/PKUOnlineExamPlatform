c0a604fb2b2d17483a6831fc143dc44288d33d87
stanley <1900012997@pku.edu.cn>
2021-11-18 13:18:55 +0800

    coding-editor

---
+++ frontend/src/router/views/coding.vue
@@ -0,0 +1,34 @@
+/* eslint-disable */
+<template>
+  <MonacoEditor
+    height="540"
+    language="python"
+    :code="code"
+    :editorOptions="options"
+    @mounted="onMounted"
+    @codeChange="onCodeChange"
+  >
+  </MonacoEditor>
+</template>
+
+<script>
+import MonacoEditor from '../../vue-monaco-editor/src/Monaco.vue'
+export default {
+  data: function () {
+    return {
+      editor: null
+    }
+  },
+  components: {
+    MonacoEditor
+  },
+  method: {
+    onMounted (editor) {
+      this.editor = editor
+    },
+    onCodeChange (editor) {
+
+    }
+  }
+}
+</script>
