c3a9cfbb0a6b8940033844e05368f70b53459b5d
wsonnnn <sll82185969@qq.com>
2021-12-19 19:46:19 +0800

    submission upload

--- backend/submission/urls.py
+++ backend/submission/urls.py
@@ -1,11 +1,11 @@
-from  django.conf.urls import url
+from django.urls import path
 
 from .views import SubmitAPI, GetSubmissionAPI, ContestSubmissionAPI, ProblemSubmissionAPI, UserSubmissionAPI
 
 urlpatterns = [
-    url(r"submission/?", SubmitAPI.as_view(), name="submission_api"),
-    url(r"getSubmission/?", GetSubmissionAPI.as_view(), name="getSubmission"),
-    url(r"contestSubmission/?", ContestSubmissionAPI.as_view(), name="contestSubmission"),
-    url(r"problemSubmission/?", ProblemSubmissionAPI.as_view(), name="problemSubmission"),
-    url(r"userSubmission/?", UserSubmissionAPI.as_view(), name="userSubmission")
+    path(r"submission/?", SubmitAPI.as_view(), name="submission_api"),
+    path(r"getSubmission/?", GetSubmissionAPI.as_view(), name="getSubmission"),
+    path(r"contestSubmission/?", ContestSubmissionAPI.as_view(), name="contestSubmission"),
+    path(r"problemSubmission/?", ProblemSubmissionAPI.as_view(), name="problemSubmission"),
+    path(r"userSubmission/?", UserSubmissionAPI.as_view(), name="userSubmission")
 ]
